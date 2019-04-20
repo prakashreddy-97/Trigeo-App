71eb200b9668050b557c47f33966d66b8d348a62
Mahesh <you@yourdomain.com>
2019-04-20 22:19:46 +0530

    Initial Commit

---
+++ Trigeo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trigeo App
+//
+//  Created by Mahesh on 20/04/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
